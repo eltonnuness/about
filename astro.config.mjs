6a7bd8104a9bfc9741da3c269cd0392e8372bc15
Elton N. <16689434+eltonnuness@users.noreply.github.com>
2024-07-26 07:55:24 -0300

    feat: configuring repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eltonnunes.github.io'
+  site: 'https://eltonnunes.github.io',
+  repo: 'about'
 });
